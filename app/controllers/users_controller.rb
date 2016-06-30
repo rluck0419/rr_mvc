9d0dc5c0589ae4da8af46d145d56ac59e31ed8ab
Robert Luckfield <rluck0419@gmail.com>
2016-06-30 08:52:55 -0500

    delete user functionality now works - all adventurer mode requirements met

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,8 @@ class UsersController < ApplicationController
   def delete
     if (1..User.all.count).include?(params[:id].to_i)
       index = params[:id].to_i - 1
-      render User.all[index].to_json
-      # render({ msg: "200 - user deleted"}.to_json, status: "200 OK")
+      User.delete_from_all(index)
+      render({ msg: "200 - user #{params[:id]} deleted"}.to_json, status: "200 OK")
     else
       render({ msg: "404 - not found" }.to_json, status: "404 NOT FOUND")
     end
